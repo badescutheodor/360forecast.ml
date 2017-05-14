c08f8ce37e99c23bb656d54be3f755d9f8b3a03f
Theodor Badescu <Theodor Badescu>
2017-05-14 18:17:22 +0300

    :thought_balloon: Fixed up mysql issue on live.

--- server/config/database.php
+++ server/config/database.php
@@ -12,7 +12,7 @@ return [
         'port'      => getenv('DATABASE_PORT') ? getenv('DATABASE_PORT') : 3306,
         'database'  => getenv('DATABASE_NAME') ? getenv('DATABASE_NAME') : '360forecast.ml',
         'username'  => getenv('DATABASE_USER') ? getenv('DATABASE_USER') : 'root',
-        'password'  => getenv('DATABASE_PASSWORD') ? getenv('DATABASE_PORT') : '',
+        'password'  => getenv('DATABASE_PASSWORD') ? getenv('DATABASE_PASSWORD') : 'ERNnfFh9Ax2PUn9NLnFW',
         'collation' => 'utf8_unicode_ci',
         'charset'   => 'utf8'
     ]
